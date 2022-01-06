b80af2eb66100a8edf313cd627ccb666fc325d2a
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-06 21:50:56 +0100

    FEAT :: added middleware to atendance endpoint for mobile device :: REFACTOR :: changed verb to GET

--- routes/attendance.js
+++ routes/attendance.js
@@ -2,11 +2,15 @@
 const express = require('express')
 const router = express.Router()
 const cors = require('cors')
-//import controllers
+//import controllers and middle ware 
 const { hardware_endpoint, mobile_endpoint } = require('../controllers/attendance')
+const { validate_auth_token, decode_jwt } = require('../middleware')
 router.use(cors())
 
-router.post('/mobile', mobile_endpoint) //mobile end point
+/*validate auth headers, verify token then give access to resource*/
+router.get('/mobile', validate_auth_token, decode_jwt, mobile_endpoint) //mobile end point
+
+/*validate POSTed data, the respond success or error*/
 router.post("/hardware" / hardware_endpoint) //hardware endpoint
 
 module.exports = router
