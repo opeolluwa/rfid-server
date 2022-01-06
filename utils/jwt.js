862c4d5ecbdc468d466176c4434e328ee73a369c
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-06 21:46:52 +0100

    REFACTOR

--- utils/jwt.js
+++ utils/jwt.js
@@ -2,7 +2,20 @@
 //load in dependencies
 const json_web_token = require('jsonwebtoken'); // generate token
 require('dotenv').config(); //load in evn variable
-// take payload, genetate token
-const jwt = (payload) => json_web_token.sign(payload, process.env.JWT_KEY);
+
+const jwt = {
+    // take payload, genetate token, it expires in six hour
+    sign: (payload) => json_web_token.sign(payload, process.env.JWT_KEY, { expiresIn: '1h' }),
+    // take payload, verify token
+    verify: (payload) => {
+        try {
+            const decoded = json_web_token.verify(payload, process.env.JWT_KEY);
+            return decoded
+        } catch (err) {
+            // err
+            return undefined
+        }
+    }
+}
 //export token
 module.exports = jwt;
