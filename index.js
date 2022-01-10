d427551d352056a297dbe0b4cf957171e426ba30
Opeoluwa Adeoye <adefemiadeoye@yahoo.com>
2022-01-10 11:46:07 +0100

    FIXIES

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 const cors = require('cors')
 
+
 //routes
 const auth = require('./routes/auth')
 const attendance = require("./routes/attendance")
--- routes/attendance.js
+++ routes/attendance.js
@@ -4,13 +4,16 @@ const router = express.Router()
 const cors = require('cors')
 //import controllers and middle ware 
 const { hardware_endpoint, mobile_endpoint } = require('../controllers/attendance')
-const { validate_auth_token, decode_jwt } = require('../middleware')
+const { validate_auth_token, decode_jwt, validate_hardware_login } = require('../middleware')
 router.use(cors())
 
 /*validate auth headers, verify token then give access to resource*/
 router.get('/mobile', validate_auth_token, decode_jwt, mobile_endpoint) //mobile end point
 
+
 /*validate POSTed data, the respond success or error*/
-router.post("/hardware" / hardware_endpoint) //hardware endpoint
+router.post("/hardware", validate_hardware_login, hardware_endpoint) //hardware endpoint
+
 
 module.exports = router
+
